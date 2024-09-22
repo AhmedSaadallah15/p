523586bfac7b123a0840daac45f1026764a8ed9f
AhmedSaadallah1 <Ahmedmohamed19982@yahoo.com>
2024-09-22 17:26:31 +0300

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,23 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { AboutComponent } from './components/about/about.component';
+import { BlogComponent } from './components/blog/blog.component';
+import { ContactComponent } from './components/contact/contact.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+
+
+  {path:'',redirectTo:'home',pathMatch:'full'},
+  {path:'home',component: HomeComponent},
+  {path:'about',component: AboutComponent},
+  {path:'portfolio',component: BlogComponent},
+  {path:'contact',component: ContactComponent}
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, {useHash: true})],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css']
+})
+export class HomeComponent {
+
+  htmlContent:string =`
+    <head>
+      <title>Ahmed Saadallah</title>
+      <link rel="stylesheet" href="s">
+    </head>
+  `;
+
+
+
+}
